d4016025eee09b87be7db9ad34dc5627c827bf49
Daniel Marques <daniel-slb-1506@hotmail.com>
2017-10-25 14:04:30 +0100

    Comentário

--- VSHelloWorld_150221093/Program.cs
+++ VSHelloWorld_150221093/Program.cs
@@ -14,6 +14,7 @@ namespace VSHelloWorld_150221093
     {
         public static void Main(string[] args)
         {
+            //Comentário de teste
             BuildWebHost(args).Run();
         }
 
